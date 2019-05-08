dc3ea083cc0b00b8ea8f7cce03d8c78ac3238a80
admin <602147184@qq.com>
2019-05-08 09:41:18 +0800

    gs--添加了save-center sale-center system-control 文件夹

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import VueRouter from 'vue-rooter'
+import VueRouter from 'vue-router'
 import routerConfig from './router.config'
 
 Vue.use(VueRouter);
