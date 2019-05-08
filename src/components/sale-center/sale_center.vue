79ea25286d96d24c09b42aa46cd4d4daadefc1cc
admin <602147184@qq.com>
2019-05-08 21:05:04 +0800

    缺陷版

--- src/components/sale-center/sale_center.vue
+++ src/components/sale-center/sale_center.vue
@@ -18,16 +18,16 @@
       </template>
     </el-menu-item>
 
-    <el-submenu index="4" class="el-submenu">
+    <el-submenu index="4" class="el-submenu" style="text-indent: 13px;">
       <template slot="title">
         <i class="el-icon-setting"></i>
         <router-link to="/saleCenter/statistics" class="router-link" style="margin-left: 7px">统计分析</router-link>
       </template>
       <el-menu-item index="4-1">
-        <router-link to="/saleCenter/statistics/sIncome" class="router-link" style="margin-left: 7px">收入统计</router-link>
+        <router-link to="/saleCenter/statistics/sIncome" class="router-link" >收入统计</router-link>
       </el-menu-item>
       <el-menu-item index="4-2">
-        <router-link to="/saleCenter/statistics/sRegister" class="router-link" style="margin-left: 7px">注册统计</router-link>
+        <router-link to="/saleCenter/statistics/sRegister" class="router-link" >注册统计</router-link>
       </el-menu-item>
     </el-submenu>
 
