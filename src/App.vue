f39a1e28b83d5b869010b6a544dab2b0bb872e7f
admin <602147184@qq.com>
2019-05-08 10:45:18 +0800

    gs--配置jq

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+<!--    <img src="./assets/logo.png">-->
     <!--<HelloWorld/>-->
+    <saleCenter></saleCenter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+// 引入销售中心
+import saleCenter from './components/sale-center/sale_center'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,abc
+    saleCenter
   }
 }
 </script>
--- src/components/sale-center/sale_center.vue
+++ src/components/sale-center/sale_center.vue
@@ -1,11 +1,40 @@
 <template>
-    
+<!--  销售中心 左边框-->
+  <div class="sale_center_main">
+    <div class="sale_center_left">
+      <div class="s-saveuser">
+        <router-link to="sSaleuser">消费用户</router-link>
+      </div>
+      <div class="s-marketuser">
+        <router-link to="sMarketuser">销售跟进用户</router-link>
+      </div>
+      <div class="order_control">
+        <router-link>订单管理</router-link>
+      </div>
+      <div class="statistics">
+        <router-link>统计分析</router-link>
+      </div>
+    </div>
+    <div class="sale_center_right">
+      saleCentermain
+    </div>
+  </div>
+
+
 </template>
 
 <script>
-    export default {
-        name: "sale_center"
+  import sMarketuser from './s-marketuser'
+  import sSaleuser from './s-saleuser'
+  import saleCentermain from './saleCentermain'
+  export default {
+    name: "sale_center",
+    components:{
+      sSaleuser,
+      sMarketuser,
+      saleCentermain
     }
+  }
 </script>
 
 <style scoped>
